8e7a4d408f6bae5bd30d81f0cdf5ac193408e380
João Marcelo Dantas <jmgrd98@gmail.com>
2024-02-26 08:10:56 -0300

    native base

--- App.tsx
+++ App.tsx
@@ -1,23 +1,27 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';}
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './components/HomeScreen';
+import AboutScreen from './components/AboutScreen';
+import CreatorScreen from './components/CreatorScreen';
+import { NativeBaseProvider } from 'native-base';
 
 const Drawer = createDrawerNavigator();
 
-// function MyDrawer() {
-//   return (
-//     <Drawer.Navigator>
-//       <Drawer.Screen name="Feed" component={} />
-//       <Drawer.Screen name="Feed" component={} />
-//     </Drawer.Navigator>
-//   )
-// }
-
 export default function App() {
   return (
-    <SafeAreaView>
-      
-    </SafeAreaView>
+    <NativeBaseProvider>
+      <SafeAreaView>
+        <NavigationContainer>
+          <Drawer.Navigator>
+            <Drawer.Screen name="Home" component={HomeScreen} />
+            <Drawer.Screen name="About" component={AboutScreen} />
+            <Drawer.Screen name="Creator" component={CreatorScreen} />
+          </Drawer.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    </NativeBaseProvider>
   );
 }
--- components/AboutScreen.tsx
+++ components/AboutScreen.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Heading } from 'native-base'
 
 const AboutScreen = () => {
   return (
     <View>
-      <Text>AboutScreen</Text>
+      <Heading>AboutScreen</Heading>
     </View>
   )
 }
